4ae0b064064f0b2b663f3a52c398467d9177e7d2
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-07-26 23:14:27 +0000

    fix style betty

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,15 +9,15 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t leaves = 0;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    if (tree->left == NULL && tree->right == NULL)
-        leaves++;
-    
-    if (tree->left != NULL)
-        leaves += binary_tree_leaves(tree->left);
-    if (tree->right != NULL)
-        leaves += binary_tree_leaves(tree->right);
-    return (leaves);
+	if (tree->left == NULL && tree->right == NULL)
+		leaves++;
+
+	if (tree->left != NULL)
+		leaves += binary_tree_leaves(tree->left);
+	if (tree->right != NULL)
+		leaves += binary_tree_leaves(tree->right);
+	return (leaves);
 }
